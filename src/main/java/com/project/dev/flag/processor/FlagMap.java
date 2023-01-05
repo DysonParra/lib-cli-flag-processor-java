9ece4c272a09ea5f72d8969195536418df414f2d
Dyson <dysontilano@gmail.com>
2023-01-05 12:20:33 -0500

    Updated license in source code files

--- src/main/java/com/project/dev/flag/processor/FlagMap.java
+++ src/main/java/com/project/dev/flag/processor/FlagMap.java
@@ -1,13 +1,16 @@
 /*
- * @fileoverview {FlagMap}, se encarga de realizar tareas especificas.
+ * @fileoverview    {FlagMap} se encarga de realizar tareas específicas.
  *
- * @version             1.0
+ * @version         2.0
  *
- * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
- * Copyright (C) Dyson Parra
+ * @author          Dyson Arley Parra Tilano <dysontilano@gmail.com>
  *
- * @history v1.0 --- La implementacion de {FlagMap} fue realizada el 30/09/2022.
- * @dev - La primera version de {FlagMap} fue escrita por Dyson A. Parra T.
+ * @copyright       Dyson Parra
+ * @see             github.com/DysonParra
+ *
+ * History
+ * @version 1.0     Implementación realizada.
+ * @version 2.0     Documentación agregada.
  */
 package com.project.dev.flag.processor;
 
